83c3e62637ecd4eb78bb97d83e85f3b4d2b0cfb7
Agus-Sosa <sosaagustin351@gmail.com>
2022-12-04 15:51:36 -0300

    Nuevos apartados de detalles de productos

--- src/containers/Itemlistcontainer.jsx
+++ src/containers/Itemlistcontainer.jsx
@@ -27,22 +27,50 @@ const Itemlistcontainer = () =>{
     }, [categoriaId])
     return (
         <>
-        <div className="portada-imagen">
+        {/* <div className="portada-imagen">
         <div data-aos="fade-up"
         data-aos-anchor-placement="bottom-bottom">
-            <div className='contenedor-imagen'>
-            <div className='texto-portada'>
-            <p className="p-portada1">Nuevo</p>
-            <h3> Iphone 14 Pro Max</h3>
-            <p className="p-portada2">Consulta por su disponibilidad</p>
-            <Link to={'item/1'}><a href="">Mas Informacion <FiChevronRight/></a></Link>
-            {/* <button>Add to cart</button> */}
+                <div className='contenedor-imagen'>
+                    <div className='texto-portada'>
+                        <p className="p-portada1">Nuevo</p>
+                        <h3> Iphone 14 Pro Max</h3>
+                        <p className="p-portada2">Consulta por su disponibilidad</p>
+                        <Link to={'item/1'}><a href="">Mas Informacion <FiChevronRight/></a></Link>
+                    </div>
+                </div>    
             </div>
-            </div>
-            
-            
+        </div>  */}
+        <div className="contenedor-carousel">
+
+            <div className="carousel w-full contenedor-carousel2">
+    <div id="slide1" className="carousel-item relative w-full">
+        <img src="https://epmgaa.media.clients.ellingtoncms.com/img/photos/2022/09/07/Apple-iPhone-14-Pro-iPhone-14-Pro-Max-hero-220907.jpg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide4" className="btn btn-circle">❮</a> 
+        <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
-        </div> 
+    </div> 
+    <div id="slide2" className="carousel-item relative w-full">
+        <img src="https://img.global.news.samsung.com/in/wp-content/uploads/2022/02/S22_Ultra_Banner_3000x2000.jpg" className="w-full" />
+        <div>
+            <h3>Samsung S22</h3>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide1" className="btn btn-circle">❮</a> 
+        <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+    </div> 
+    <div id="slide3" className="carousel-item relative w-full">
+        <img src="https://i01.appmifile.com/webfile/globalimg/products/m/xiaomi-11t/section3_video.jpg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide2" className="btn btn-circle">❮</a> 
+        <a href="#slide4" className="btn btn-circle">❯</a>
+        </div>
+    </div> 
+
+    </div>
+        </div>
+
         <div className='titulo-productos'>
             <h1>Productos Destacados</h1>
         </div> 
@@ -52,9 +80,9 @@ const Itemlistcontainer = () =>{
         </div>
 
         {/* {
-
-        
-        
+            
+            
+            
     data.map(item =>(
         <Item 
         
