15d6302bd80154831583940fb209d1ebefea8c67
Agus-Sosa <sosaagustin351@gmail.com>
2023-03-31 16:56:06 -0300

    Solucion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ItemDetailContainer from "./containers/ItemDetailContainer";
 import CartContextProvider from "./components/context/CartContext";
-import Cart from "./components/cart";
 import './App.css'
 const App = () =>{
   return (
@@ -17,7 +16,6 @@ const App = () =>{
           <Route path="/"element={<Itemlistcontainer/>}/>
           <Route path="/categoria/:categoriaId" element={<Itemlistcontainer/>}/>
           <Route path="/item/:idProducto" element={<ItemDetailContainer/>}/> 
-          <Route path="/carrito" element={<Cart/>} />
         </Routes>
       <Footer/>
     </BrowserRouter>
